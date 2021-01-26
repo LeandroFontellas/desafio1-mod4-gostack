473046d3a00f56b27eb60a45b2950324b2aed1fe
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2021-01-26 16:37:55 -0300

    feat(customers): criada funcionalidade de adicionar um customer via http

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkEmailAvailability = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkEmailAvailability) {
+      throw new AppError('Email is already taken');
+    }
+
+    const customer = await this.customersRepository.create({ email, name });
+
+    return customer;
   }
 }
 
