e9b433457b3523bd6926e172ac937b7c0d2fc057
Leandro Fontellas Laurito <leandrofontellas@gmail.com>
2021-01-27 09:00:54 -0300

    feat(products): criada a funcionalidade para adicionar um produto novo ao banco

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -4,24 +4,33 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
-  OneToMany,
+  // OneToMany,
 } from 'typeorm';
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('products')
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   price: number;
 
+  @Column()
   quantity: number;
 
+  // @OneToMany(() => OrdersProducts)
+  // @JoinColumn({ name: 'provider_id' })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
